bfe62dc2a597203239be48db3a0068fbd83de958
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-11-25 14:38:08 +0300

    Input binding - v-model form-modifier

---
+++ src/App_v1.vue
@@ -0,0 +1,79 @@
+<template>
+    <div id="app">
+        <app-header
+            v-bind:title="title"
+            v-on:changeTitle="updateTitle($event)"
+        ></app-header>
+        <!-- <app-daniels v-bind:daniels="daniels"></app-daniels> -->
+        <keep-alive>       
+            <component v-bind:is="component"></component>
+        </keep-alive>
+        <button v-on:click="component = 'form-one'">Show Form One</button>
+        <button v-on:click="component = 'form-two'">Show Form Two</button>
+        <!-- <form-one></form-one> -->
+        <!-- <form-two></form-two> -->
+        <app-footer v-bind:title_footer="title"></app-footer>
+    </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+//import Daniels from "./components/Dan";
+import formOne from "./components/formOne";
+import formTwo from "./components/formTwo";
+
+export default {
+    name: "app",
+    components: {
+        "app-header": Header,
+        "app-footer": Footer,
+        //"app-daniels": Daniels,
+        "form-one": formOne,
+        "form-two": formTwo,
+    },
+    data() {
+        return {
+            daniels: [
+                {
+                    name: "Elias Gitonga",
+                    relationship: "Grandfather",
+                    show: false,
+                },
+                {
+                    name: "Caroline Kanana",
+                    relationship: "Grandmother",
+                    show: false,
+                },
+                { name: "Daniel Mutwiri", relationship: "Father", show: false },
+                { name: "Ann Mutwiri", relationship: "Mother", show: false },
+                { name: "Bryan Muriiki", relationship: "Uncle", show: false },
+                { name: "JoyWendy Gitonga", relationship: "Aunt", show: false },
+                {
+                    name: "Stephanie Mutwiri",
+                    relationship: "daughter",
+                    show: false,
+                },
+            ],
+            title: "Leaning Vue",
+            formHelper: "This is the form helper details",
+            component: "form-two"
+        };
+    },
+    methods: {
+        updateTitle: function(updatedTitle) {
+            this.title = updatedTitle;
+        },
+    },
+};
+</script>
+
+<style scoped>
+h1 {
+    color: purple;
+}
+body {
+    margin: 0;
+    font-family: "Nunito SemiBold";
+}
+</style>
---
+++ src/components/addBlog.vue
@@ -0,0 +1,74 @@
+<template>
+    <div id="add-blog">
+        <h2>Add a new Blog Post</h2>
+        <form>
+            <label for="title"></label>
+            <input v-model.lazy="blog.title" type="text" id="title" name="title" required />
+            <label for="content"></label>
+            <textarea
+                v-model.lazy="blog.content"
+                name="blog_content"
+                id="content"
+                cols="30"
+                rows="10">
+            </textarea>
+            <input type="button" value="save blog" v-on:click="submitForm()">
+        </form>
+        <div id="preview">
+          <h3>Preview Blog</h3>
+          <p>Blog title: <span id="preview-text">{{ blog.title }}</span></p>
+          <p>Blog content:</p>
+          <p>{{ blog.content }}</p>
+        </div>
+        <div></div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blog: {
+        title: '',
+        content: ''
+      }
+    }
+  },
+  methods: {
+    submitForm: function() {
+      //
+    }
+  },
+};
+</script>
+
+<style>
+#add-blog *{
+    box-sizing: border-box;
+}
+#add-blog{
+    margin: 20px auto;
+    max-width: 500px;
+}
+label{
+    display: block;
+    margin: 20px 0 10px;
+}
+input[type="text"], textarea{
+    display: block;
+    width: 100%;
+    padding: 8px;
+}
+#preview{
+    padding: 10px 20px;
+    border: 1px dotted #ccc;
+    margin: 30px 0;
+}
+#preview-text {
+  font-weight: bold;
+}
+h3{
+    margin-top: 10px;
+}
+
+</style>
