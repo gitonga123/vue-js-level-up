c43a38693a789d9bff28de3a2cee54bb0ec99d8e
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-11-25 13:58:53 +0300

    Vue - Slots

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
     <div id="app">
         <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
-        <app-daniels v-bind:daniels="daniels"></app-daniels>
+        <!-- <app-daniels v-bind:daniels="daniels"></app-daniels> -->
+        <form-helper>
+            <div slot="form-header">
+                <h3>This is the title of the form</h3>
+                <p>Information about the form</p>
+            </div>
+            <div slot="form-fields">
+                <input type="text" name="user_name" placeholder="username" required>
+                <input type="password" name="password" placeholder="Password" required>
+            </div>
+            <div slot="form-controls">
+                <input type="button" value="Submit" @click="submitForm">
+            </div>
+        </form-helper>
         <app-footer v-bind:title_footer="title"></app-footer>
     </div>
 </template>
@@ -9,14 +22,16 @@
 <script>
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Daniels from "./components/Dan";
+//import Daniels from "./components/Dan";
+import formHelper from './components/formHelper'
 
 export default {
     name: "app",
     components: {
         "app-header": Header,
         "app-footer": Footer,
-        "app-daniels": Daniels,
+        //"app-daniels": Daniels,
+        "form-helper": formHelper
     },
     data() {
         return {
@@ -29,12 +44,16 @@ export default {
                 { name: "JoyWendy Gitonga", relationship: "Aunt", show: false },
                 { name: "Stephanie Mutwiri", relationship: "daughter", show: false }
             ],
-            title: "Leaning Vue"
+            title: "Leaning Vue",
+            formHelper: "This is the form helper details"
         }
     },
     methods: {
         updateTitle: function(updatedTitle) {
             this.title = updatedTitle;
+        },
+        submitForm: function() {
+            alert('Form submitted');
         }
     }
 };
@@ -43,5 +62,9 @@ export default {
 <style scoped>
 h1 {
     color: purple;
+}
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
 }
 </style>
