b8105bc8adc1076b67a8d7b5e3c5b6ffed2ab454
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-11-25 14:12:55 +0300

    Vue - Dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
     <div id="app">
-        <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
+        <app-header
+            v-bind:title="title"
+            v-on:changeTitle="updateTitle($event)"
+        ></app-header>
         <!-- <app-daniels v-bind:daniels="daniels"></app-daniels> -->
-        <form-helper>
-            <div slot="form-header">
-                <h3>This is the title of the form</h3>
-                <p>Information about the form</p>
-            </div>
-            <div slot="form-fields">
-                <input type="text" name="user_name" placeholder="username" required>
-                <input type="password" name="password" placeholder="Password" required>
-            </div>
-            <div slot="form-controls">
-                <input type="button" value="Submit" @click="submitForm">
-            </div>
-        </form-helper>
+        <keep-alive>       
+            <component v-bind:is="component"></component>
+        </keep-alive>
+        <button v-on:click="component = 'form-one'">Show Form One</button>
+        <button v-on:click="component = 'form-two'">Show Form Two</button>
+        <!-- <form-one></form-one> -->
+        <!-- <form-two></form-two> -->
         <app-footer v-bind:title_footer="title"></app-footer>
     </div>
 </template>
@@ -23,7 +20,8 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 //import Daniels from "./components/Dan";
-import formHelper from './components/formHelper'
+import formOne from "./components/formOne";
+import formTwo from "./components/formTwo";
 
 export default {
     name: "app",
@@ -31,31 +29,42 @@ export default {
         "app-header": Header,
         "app-footer": Footer,
         //"app-daniels": Daniels,
-        "form-helper": formHelper
+        "form-one": formOne,
+        "form-two": formTwo,
     },
     data() {
         return {
             daniels: [
-                { name: "Elias Gitonga", relationship: "Grandfather", show: false },
-                { name: "Caroline Kanana", relationship: "Grandmother", show: false },
-                { name: "Daniel Mutwiri", relationship: "Father", show:false },
-                { name: "Ann Mutwiri", relationship: "Mother", show: false},
+                {
+                    name: "Elias Gitonga",
+                    relationship: "Grandfather",
+                    show: false,
+                },
+                {
+                    name: "Caroline Kanana",
+                    relationship: "Grandmother",
+                    show: false,
+                },
+                { name: "Daniel Mutwiri", relationship: "Father", show: false },
+                { name: "Ann Mutwiri", relationship: "Mother", show: false },
                 { name: "Bryan Muriiki", relationship: "Uncle", show: false },
                 { name: "JoyWendy Gitonga", relationship: "Aunt", show: false },
-                { name: "Stephanie Mutwiri", relationship: "daughter", show: false }
+                {
+                    name: "Stephanie Mutwiri",
+                    relationship: "daughter",
+                    show: false,
+                },
             ],
             title: "Leaning Vue",
-            formHelper: "This is the form helper details"
-        }
+            formHelper: "This is the form helper details",
+            component: "form-two"
+        };
     },
     methods: {
         updateTitle: function(updatedTitle) {
             this.title = updatedTitle;
         },
-        submitForm: function() {
-            alert('Form submitted');
-        }
-    }
+    },
 };
 </script>
 
@@ -63,8 +72,8 @@ export default {
 h1 {
     color: purple;
 }
-body{
+body {
     margin: 0;
-    font-family: 'Nunito SemiBold';
+    font-family: "Nunito SemiBold";
 }
 </style>
