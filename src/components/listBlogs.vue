cc5b558ef08fb485dc3ddcc9ec9ce170485bbb9d
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-11-26 15:29:59 +0300

    Vue global and local directs - refactor

--- src/components/listBlogs.vue
+++ src/components/listBlogs.vue
@@ -4,7 +4,7 @@
         <input type="text" v-model="search" placeholder="search blogs">
         <div v-for="(blog, key) in filterBlogs" class="single-blog" :key="key">
             <h3 v-rainbow>{{ blog.title|to-uppercase }}</h3>
-            <article>{{ blog.body }}</article>
+            <article>{{ blog.body|snippet }}</article>
         </div>
     </div>
 </template>
@@ -33,6 +33,37 @@ export default {
             })
         }
     },
+    filters: {
+        toUppercase(value) {
+            return value.toUpperCase();
+        },
+        snippet(value) {
+            return value.slice(0, 100) + '...';
+        }
+    },
+    directives: {
+        'rainbow': {
+            bind(el, binding, vnode) {
+                el.style.color = "#" + Math.random().toString().slice(2, 8);
+                binding;
+                vnode;
+            }
+        },
+        'theme': {
+            bind(el, binding, vnode) {
+                if (binding.value == 'wide') {
+                    el.style.maxWidth = '1500px';
+                } else if (binding.value == 'narrow') {
+                    el.style.maxWidth = '750px';
+                }
+                if (binding.arg == 'column') {
+                    el.style.background = "#ddd";
+                    el.style.padding = "20px";
+                }
+                vnode;
+            }
+        }
+    }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -8,13 +8,6 @@ Vue.use(VueResource);
 export const bus = new Vue();
 
 // Custom directives
-Vue.directive('rainbow', {
-  bind(el, binding, vnode) {
-    el.style.color = "#" + Math.random().toString().slice(2, 8);
-    binding;
-    vnode;
-  }
-});
 
 Vue.directive('theme', {
   bind(el, binding, vnode) {
@@ -31,13 +24,6 @@ Vue.directive('theme', {
   }
 });
 
-// filters
-Vue.filter('to-uppercase', (value) => {
-  return value.toUpperCase();
-});
-Vue.filter('snippet', (value) => {
-  return value.slice(0, 100) + '...';
-})
 
 new Vue({
   render: h => h(App),
