17d58f6590cb4c91ebd4ca8dc1a4064f8a9bb6ce
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-11-29 16:14:50 +0300

    Vue js - Vuex mutations

--- src/components/productListOne.vue
+++ src/components/productListOne.vue
@@ -7,6 +7,7 @@
                 <span class="price">Ksh. {{ product.price }}</span>
             </li>
         </ul>
+        <button @click="reducePrice">Reduce Price</button>
     </div>
 </template>
 
@@ -20,6 +21,11 @@ export default {
             return this.$store.getters.saleProducts;
         }
     },
+    methods: {
+        reducePrice: function() {
+            this.$store.commit('reducePrice');
+        }
+    }
 }
 </script>
 
